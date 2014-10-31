caff40481cb4900c0d8d0ddbe7309dbc2f595e7a
rouber_l <leo.roubertoux@epitech.eu>
2014-10-31 14:16:42 +0100

    add hash table

---
+++ loginscreen/hash.cpp
@@ -0,0 +1,28 @@
+#include "Hash.h"
+
+
+int	Hash::hashing(char *string)
+{
+	int		hashed;
+	int		count = 0;
+
+	hashed = this->offset_basis;
+	while (string[count])
+	{
+		hashed = hashed ^ string[count];
+		hashed = hashed * this->FNV_prime;
+		count++;
+	}
+	return (hashed);
+}
+
+Hash::Hash()
+{
+	this->FNV_prime = 16777619;
+	this->offset_basis = 2166136261;
+}
+
+
+Hash::~Hash()
+{
+}
--- loginscreen/main.cpp
+++ loginscreen/main.cpp
@@ -1,10 +1,16 @@
 #include "loginscreen.h"
+#include "Hash.h"
 #include "iostream"
+
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	LoginScreen	logScreen;
+	Hash		toto;
+	int			result;
 
+	result = toto.hashing("");
+	std::cout << "Test  " << result << std::endl;
 	logScreen.show();
 	return a.exec();
 }
